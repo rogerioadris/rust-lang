1609330cd99771cffd2fc7d3b158d478a01b5737
Rogério Adriano <rogerioadris@gmail.com>
2021-01-30 17:35:38 -0300

    Refatorar Teste

--- lib/common/src/byte.rs
+++ lib/common/src/byte.rs
@@ -22,7 +22,7 @@ impl<'l> Byte<'l> {
         self.bit
     }
 
-    pub fn change(&mut self, alias: &str, value: bool) -> &Byte<'l> {
+    pub fn change(&mut self, alias: &str, value: bool) -> &mut Byte<'l> {
         match self.alias.iter().position(|&s| s == alias) {
             Some(i) => match i {
                 0..=7 => match value {
@@ -35,6 +35,14 @@ impl<'l> Byte<'l> {
         };
 
         self
+    }
+
+    pub fn turn_on(&mut self, alias: &str) -> &mut Byte<'l> {
+        self.change(alias, true)
+    }
+
+    pub fn turn_off(&mut self, alias: &str) -> &mut Byte<'l> {
+        self.change(alias, false)
     }
 
     pub fn all(&mut self) -> &mut Byte<'l> {
@@ -55,60 +63,32 @@ mod tests {
     #[test]
     fn it_works() {
         let mut byte = Byte::default();
+
+        // Inicial
         assert_eq!(byte.number(), 0);
 
-        byte.change("b1", true);
-        assert_eq!(byte.number(), 1);
+        // On
+        assert_eq!(byte.turn_on("b1").number(), 1);
+        assert_eq!(byte.turn_on("b2").number(), 3);
+        assert_eq!(byte.turn_on("b3").number(), 7);
+        assert_eq!(byte.turn_on("b4").number(), 15);
+        assert_eq!(byte.turn_on("b5").number(), 31);
+        assert_eq!(byte.turn_on("b6").number(), 63);
+        assert_eq!(byte.turn_on("b7").number(), 127);
+        assert_eq!(byte.turn_on("b8").number(), 255);
 
-        byte.change("b2", true);
-        assert_eq!(byte.number(), 3);
+        // Off
+        assert_eq!(byte.turn_off("b1").number(), 254);
+        assert_eq!(byte.turn_off("b2").number(), 252);
+        assert_eq!(byte.turn_off("b3").number(), 248);
+        assert_eq!(byte.turn_off("b4").number(), 240);
+        assert_eq!(byte.turn_off("b5").number(), 224);
+        assert_eq!(byte.turn_off("b6").number(), 192);
+        assert_eq!(byte.turn_off("b7").number(), 128);
+        assert_eq!(byte.turn_off("b8").number(), 0);
 
-        byte.change("b3", true);
-        assert_eq!(byte.number(), 7);
-
-        byte.change("b4", true);
-        assert_eq!(byte.number(), 15);
-
-        byte.change("b5", true);
-        assert_eq!(byte.number(), 31);
-
-        byte.change("b6", true);
-        assert_eq!(byte.number(), 63);
-
-        byte.change("b7", true);
-        assert_eq!(byte.number(), 127);
-
-        byte.change("b8", true);
-        assert_eq!(byte.number(), 255);
-
-        byte.change("b1", false);
-        assert_eq!(byte.number(), 254);
-
-        byte.change("b2", false);
-        assert_eq!(byte.number(), 252);
-
-        byte.change("b3", false);
-        assert_eq!(byte.number(), 248);
-
-        byte.change("b4", false);
-        assert_eq!(byte.number(), 240);
-
-        byte.change("b5", false);
-        assert_eq!(byte.number(), 224);
-
-        byte.change("b6", false);
-        assert_eq!(byte.number(), 192);
-
-        byte.change("b7", false);
-        assert_eq!(byte.number(), 128);
-
-        byte.change("b8", false);
-        assert_eq!(byte.number(), 0);
-
-        byte.all().change("b4", false);
-        assert_eq!(byte.number(), 240);
-
-        byte.clean().change("b4", true);
-        assert_eq!(byte.number(), 8);
+        // Todos / Nenhum
+        assert_eq!(byte.all().number(), 255);
+        assert_eq!(byte.clean().number(), 0);
     }
 }
